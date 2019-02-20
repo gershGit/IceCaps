6f0f66996952217f5bd307ad5c400c4a98e72697
Garret Gershmel <gershmail1@gmail.com>
2019-02-20 12:10:15 -0600

    Removed Unsupported Engine Builds

--- Engine Builds/VulkanProject/Headers/Headers.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <vulkan\vulkan.h>
-#include <vector>
-#include <iostream>
-#include <memory>
-#include <mutex>
-#include <assert.h>
-#include <GLFW/glfw3.h>
-#include <windows.h>
